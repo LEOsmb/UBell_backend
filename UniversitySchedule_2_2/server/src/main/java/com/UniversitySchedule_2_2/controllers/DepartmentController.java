bf918786363f96d71303e6644df9edf7b559fbe4
LEOsmb <44555690+LEOsmb@users.noreply.github.com>
2019-10-23 18:45:30 +0300

    Update DepartmentController. Add alot of methods. Like a deleteExhibitAuthor, save, deleteById, getOneDepartment

--- UniversitySchedule_2_2/server/src/main/java/com/UniversitySchedule_2_2/controllers/DepartmentController.java
+++ UniversitySchedule_2_2/server/src/main/java/com/UniversitySchedule_2_2/controllers/DepartmentController.java
@@ -28,6 +28,67 @@ public class DepartmentController {
         return departmentService.getAllDepartments();
     }
 
+      @Override
+    public boolean addExhibitAuthor(Author author, Exhibit exhibit) {
+        if ((authorDao.findById(author.getId()).isPresent()) &&
+                (exhibitDao.findById(exhibit.getId()).isPresent())) {
+            throw new NotSavedException(ErrorMessage.EXHIBIT_AUTHOR_NOT_DELETED);
+        } else {
+            authorDao.addAuthor(author, exhibit);
+            return true;
+        }
+    }
+
+    /**
+     * Method for deleting Exhibit's Author
+     *
+     * @param author Author which must be deleted
+     * @param exhibit Exhibit for which yow want to delete Author
+     * @return true if the delete was successful
+     */
+    @Override
+    public boolean deleteExhibitAuthor(Author author, Exhibit exhibit) {
+        if ((authorDao.findById(author.getId()).isPresent()) &&
+                (exhibitDao.findById(exhibit.getId()).isPresent())) {
+            authorDao.deleteAuthor(author, exhibit);
+            return true;
+        } else {
+            throw new NotDeletedException(ErrorMessage.EXHIBIT_AUTHOR_NOT_DELETED);
+        }
+    }
+
+    /**
+     * Method for saving objects in database
+     *
+     * @return true if the save was successful
+     */
+    @Override
+    public boolean save(AuthorDto authorDto) {
+        if (authorDao.findByFullName(authorDto.getFirstName(), authorDto.getLastName())
+                .isPresent()) {
+            throw new NotSavedException(ErrorMessage.AUTHOR_NOT_SAVED);
+        } else {
+            Author author = new Author(authorDto.getFirstName(), authorDto.getLastName());
+            authorDao.save(author);
+            return true;
+        }
+    }
+
+    /**
+     * Method for deleting object Author by id
+     *
+     * @return true if the delete was successful
+     */
+    @Override
+    public boolean deleteById(long id) {
+        if (authorDao.findById(id).isPresent()) {
+            authorDao.deleteById(id);
+            return true;
+        } else {
+            throw new NotDeletedException(ErrorMessage.AUTHOR_NOT_DELETED);
+        }
+    }
+    
     @GetMapping("/{id}")
     public DepartmentDTO getOneDepartment(@PathVariable Long id) {
         return departmentService.getOneDepartment(id);
